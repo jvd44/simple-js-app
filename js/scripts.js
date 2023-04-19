4fe270ba04e9ce5bf42b3551c68d1057cfee7508
jvd44 <jvdembski@gmail.com>
2023-04-19 12:46:13 -0400

    Update scripts.js
    Changed "water" to 'water' in the type describing squirtle

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,1 @@
-let pokemonList = [{name: 'bulbasaur', height: 7, type: ['grass','poison']}, {name: 'charmander', height: 6, type: "fire"}, {name: 'squirtle', height: 5, type: water}]
+let pokemonList = [{name: 'bulbasaur', height: 7, type: ['grass','poison']}, {name: 'charmander', height: 6, type: 'fire'}, {name: 'squirtle', height: 5, type: 'water'}]
