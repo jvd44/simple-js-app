b1da190699d2d00833863e2378d38247cbb1c4c5
jvd44 <jvdembski@gmail.com>
2023-06-21 11:40:37 -0400

    Replaced for loop with forEach function
    Replaced for loop iterating over each pokemon in array with forEach function that does the same thing but cleaner.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,27 @@
 let pokemonList = [
-  {
-  name: 'Bulbasaur',
-  height: 0.7,
-  type: ['grass','poison']
-  }, 
-  {
-  name: 'Charmander',
-  height: 0.6,
-  type: 'fire',
-  }, 
-  {
-  name: 'Squirtle',
-  height: 0.5,
-  type: 'water',
-  }
+    {
+    name: 'Bulbasaur',
+    height: 0.7,
+    type: ['grass','poison']
+    }, 
+    {
+    name: 'Charmander',
+    height: 0.6,
+    type: 'fire',
+    }, 
+    {
+    name: 'Squirtle',
+    height: 0.5,
+    type: 'water',
+    }
 ];
 
 
 for (let i = 0; i < pokemonList.length; i++){
-  document.write(`<p>${pokemonList[i].name + " " }`)
-  document.write(`(height: ${pokemonList[i].height}")` + " ")
-  if (pokemonList[i].height >= 0.7){
-      document.write(`Wow, that's big!` + " ");
-  }
+    document.write(`<p>${pokemonList[i].name + " " }`)
+    document.write(`(height: ${pokemonList[i].height}")` + " ")
+    if (pokemonList[i].height >= 0.7){
+        document.write(`Wow, that's big!` + " ");
+    }
 };
+
