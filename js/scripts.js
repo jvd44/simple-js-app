d4ce74c3820ee93f301a7d4430cd0fd7b8e34956
jvd44 <jvdembski@gmail.com>
2023-06-21 12:10:32 -0400

    Created IIFE to nest pokemon array
    Created and IIFE to protect pokemon array. Also created a getAll function to return array, a add function to be able to add pokemon to array, and a return function to be able to access array outside of IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,37 @@
-let pokemonList = [
-    {
-    name: 'Bulbasaur',
-    height: 0.7,
-    type: ['grass','poison']
-    }, 
-    {
-    name: 'Charmander',
-    height: 0.6,
-    type: 'fire',
-    }, 
-    {
-    name: 'Squirtle',
-    height: 0.5,
-    type: 'water',
+let pokemonRepository = (function () { //Wrap array in IIFE
+
+    let pokemonList = [
+        {
+        name: 'Bulbasaur',
+        height: 0.7,
+        type: ['grass','poison']
+        }, 
+        {
+        name: 'Charmander',
+        height: 0.6,
+        type: 'fire',
+        }, 
+        {
+        name: 'Squirtle',
+        height: 0.5,
+        type: 'water',
+        }
+    ]
+    function getAll () {//returns all pokemon in pokemonList array
+        return pokemonList;
     }
-];
+    function add (pokemon) {//adds pokemon to pokemon array
+        pokemonList.push(pokemon);
+    }
+    return { // returns functions to be used outside IIFE
+        getAll: getAll,
+        add: add
+    }
+})();
 
-
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
     document.write("<p>" + pokemon.name + " " + `(height: ${pokemon.height}")` + "  </p>")
 });
-
 /*
 for (let i = 0; i < pokemonList.length; i++){
     document.write(`<p>${pokemonList[i].name + " " }`)
